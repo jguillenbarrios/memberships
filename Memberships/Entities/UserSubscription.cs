6555c7d58fb633a46ef9b39609b58eb5b9a4abb5
Jesus Guillen <jesus.guillen@nielsen.com>
2017-05-06 01:06:44 -0500

    first

---
+++ Memberships/Entities/UserSubscription.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Memberships.Entities
+{
+    [Table("UserSubscription")]
+    public class UserSubscription
+    {
+        //Composite Key: Subscription & AspNetUser tables
+        [Key, Column(Order = 1)]
+        [Required]
+        public int SubscriptionId { get; set; }
+
+        [Key, Column(Order = 2)]
+        [MaxLength(128)]
+        [Required]
+        public string UserId { get; set; }
+
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+    }
+}
